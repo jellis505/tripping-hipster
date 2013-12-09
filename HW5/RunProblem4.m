494702a8307df64dcaa2cc8f1bf85eb41b4dc14d
Joe Ellis <joe.ellis.505@gmail.com>
2013-12-09 18:42:32 -0500

    Added some changes to the code, and now it runs great.  Plus added some changes to Problem 1

--- HW5/RunProblem4.m
+++ HW5/RunProblem4.m
@@ -7,10 +7,28 @@ close all; clear all;
 % We will be building the junction tree algorithm for markov chain
 % First initalize the values of each cell, choose the number of the markov
 % chain in the line below equal to n
+
+% This variable states how long we want the chain to be
 n = 5;
-psis = cell(n-1, 1);
-for i = 1:(n-1)
-    psis{i} = rand(2,2);
+
+% This variable controls if we want to run the test discussed in the HW
+% test = 1 -- run the values assigned in the HW5 Problem 4
+% test = 0 -- means run the JTA with randomly assigned values
+test = 1;
+
+if test == 1
+    psis = cell(n-1,1);
+    psis{1} = [0.1 0.7;0.8 0.3];
+    psis{2} = [0.5 0.1;0.1 0.5];
+    psis{3} = [0.1 0.5;0.5 0.1];
+    psis{4} = [0.9 0.3;0.1 0.3];
+    disp('Running with pre-determined psis')
+else
+    disp('Running with random preinitialized psis')
+    psis = cell(n-1, 1);
+    for i = 1:(n-1)
+        psis{i} = rand(2,2);
+    end
 end
 
 % Now we should initalize the sperators... Which are called phis, as 1's
@@ -40,7 +58,7 @@ for i = n-2:-1:1
     phi_star = psis{i+1}(:,1) + psis{i+1}(:,2);
     % Now go through the first values and top row of the psis value
     for j = 1:2
-        psis{i}(:,j) = (phi_star(j)/phis{i}(j))*psis{i+1}(:,j);
+        psis{i}(:,j) = (phi_star(j)/phis{i}(j))*psis{i}(:,j);
     end
     phis{i} = phi_star';
 end
