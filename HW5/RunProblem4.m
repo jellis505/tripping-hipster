0dc8b1807594b12bf6184453bc1a109a94f5fa8c
Joe Ellis <joe.ellis.505@gmail.com>
2013-12-09 09:41:22 -0500

    Added the file for problem 4

---
+++ HW5/RunProblem4.m
@@ -0,0 +1,55 @@
+% This script runs problem 4 for the HW5 in Machine Learning
+% Created by Joe Ellis
+% Columbia University -- Fall 2013
+
+
+close all; clear all;
+% We will be building the junction tree algorithm for markov chain
+% First initalize the values of each cell, choose the number of the markov
+% chain in the line below equal to n
+n = 5;
+psis = cell(n-1, 1);
+for i = 1:(n-1)
+    psis{i} = rand(2,2);
+end
+
+% Now we should initalize the sperators... Which are called phis, as 1's
+phis = cell(n-2,1);
+for i = 1:n-2
+    phis{i} = ones(1,2);
+end
+
+% Now that the psis and phis are initialized let's perform the JTA
+% aglorithm
+% Left - Right algorithm pass
+for i = 1:n-2
+    % Add the first row to the second row
+    phi_star = psis{i}(1,:) + psis{i}(2,:);
+    % Do the first values top row of the psis value
+    for j = 1:2
+        psis{i+1}(j,:) = (phi_star(j)/phis{i}(j))*psis{i+1}(j,:);
+    end
+    phis{i} = phi_star;
+end
+
+% Now let's do the Right - Left Algorithm pass
+% We will pass through this backwards
+for i = n-2:-1:1
+    % Add the first row to the second column, this is because we are moving
+    % backward through the JTA algorithm
+    phi_star = psis{i+1}(:,1) + psis{i+1}(:,2);
+    % Now go through the first values and top row of the psis value
+    for j = 1:2
+        psis{i}(:,j) = (phi_star(j)/phis{i}(j))*psis{i+1}(:,j);
+    end
+    phis{i} = phi_star';
+end
+
+% Let's normalize each of the values of each psi, because 
+for k = 1:n-1
+    psis{k} = psis{k}/(sum(psis{k}(:)));
+    psis{k}
+end
+
+
+    
